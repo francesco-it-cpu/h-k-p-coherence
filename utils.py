1a56640a3dd704311a400131d7a7d56a3af8a834
flenz00 <flavio.bava00@gmail.com>
2023-08-23 11:40:58 +0200

    Added Datasets pub and priv

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
 
-def read_file():
-    return
+def read_datasets(pub_path,priv_path):
+    pub = open(pub_path).read()
+    priv = open(priv_path).read()
+    return [pub,priv]
+
+if __name__ == '__main__':
+    print(read_datasets('./Datasets/pub.dat','./Datasets/priv.dat'))
